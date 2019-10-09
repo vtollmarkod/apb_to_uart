abb6f79c25e050460f3405112a10fe59c08efeb5
markod <markod@thevtool.com>
2019-10-09 17:55:47 +0300

    Check UART monitor

--- apb_agent/apb_sequence_item.sv
+++ apb_agent/apb_sequence_item.sv
@@ -7,7 +7,7 @@ class apb_sequence_item extends uvm_sequence_item;
 	rand bit [31:0] pwdata;
   	rand rw_mode mode;
 	rand int delay;
-  	rand bit [31:0] prdata; // Only using rand to constraint to 32'h0
+  		 bit [31:0] prdata; // Only using rand to constraint to 32'h0
 
 	`uvm_object_utils_begin(apb_sequence_item)
 	`uvm_field_int(paddr,UVM_ALL_ON)
@@ -23,9 +23,8 @@ class apb_sequence_item extends uvm_sequence_item;
 
 
 	constraint apb_sequence_item {
-      	paddr inside {[1:100]};
-      	prdata == 32'hffff_ffff;
-      	pwdata inside {[400:500]};
-		delay inside {[5:10]};
+      	paddr inside {[32'h0000_0000:32'hc000_0000]}; // all registers
+      	//pwdata inside {[400:500]};
+		delay inside {[0:50]};
 	}
 endclass:apb_sequence_item
--- test_pkg/my_virtual_sequence.sv
+++ test_pkg/my_virtual_sequence.sv
@@ -6,7 +6,7 @@ class my_virtual_sequence extends uvm_sequence;
     super.new(name);
   endfunction
   
-  
+  uart_write_sequence uart_write_sequence_vh;
   apb_read_sequence 	apb_read_sequence_vh;
   apb_write_sequence	apb_write_sequence_vh;
 
@@ -14,14 +14,15 @@ class my_virtual_sequence extends uvm_sequence;
   task pre_body();
   	apb_read_sequence_vh = apb_read_sequence::type_id::create("apb_read_sequence_vh");
   	apb_write_sequence_vh = apb_write_sequence::type_id::create("apb_write_sequence_vh");
+    uart_write_sequence_vh = uart_write_sequence :: type_id::create("uart_write_sequence_vh");
   endtask
 
   // A handle called p_sequencer is created within the sequence via macro `uvm_declare_p_sequencer 
   // and assigned to be run with my_virtual_sequencer
 
   task body();
-    repeat(6)
-      apb_write_sequence_vh.start(p_sequencer.apb_sequencer_h);
+    repeat(1)
+      uart_write_sequence_vh.start(p_sequencer.apb_sequencer_h);
   endtask
 
 endclass:my_virtual_sequence
